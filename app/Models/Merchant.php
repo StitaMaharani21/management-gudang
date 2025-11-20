033d73f46a99b689e500d9c07a984daef00edd16
StitaMaharani21 <stitamaharanigst@gmail.com>
2025-11-20 14:54:59 +0700

    model

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Merchant extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'address',
+        'phone',
+        'keeper_id',
+    ];
+
+    public function keeper()
+    {
+        return $this->belongsTo(User::class, 'keeper_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'merchant_products')->withPivot('stock')->withTimestamps();
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function getPhotoUrlAttribute($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        return url(Storage::url($value));
+    }
 }
