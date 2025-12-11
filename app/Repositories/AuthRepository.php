9d913de227ce5b64b2cfbe552cbf84c7975d1819
StitaMaharani21 <stitamaharanigst@gmail.com>
2025-12-12 00:28:06 +0700

    api

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -55,19 +55,4 @@ class AuthRepository
 
         return $token;
     }
-        ];
-
-        if (!Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
-        }
-
-        $user = Auth::user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Login successful',
-            'token' => $token,
-            'user' => new UserResource($user->load('roles')),
-        ], 200);
-    }
 }
